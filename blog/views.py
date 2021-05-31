d01605cbd7326efc006955dd0463d7ebbe27cf18
unknown <maxzah99@gmail.com>
2021-05-31 22:01:02 +0300

    2 webpag

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+def showblog(request):
+	posts = Blog.objects
+	return render(request, 'blog/blog.html', {'posts':posts})
